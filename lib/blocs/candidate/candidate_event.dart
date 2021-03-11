d57498c8ea3a7086f7bfccb0f252ef039d644823
solkn <solomonkindie52@gmail.com>
2021-03-12 00:01:57 +0300

    candidate_event.dart

---
+++ lib/blocs/candidate/candidate_event.dart
@@ -0,0 +1,34 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:voting_system_flutter/models/model.dart';
+
+abstract class CandidateEvents extends Equatable{}
+
+class GetCandidateEvent extends CandidateEvents{
+  GetCandidateEvent();
+  List<Object> get props=>[];
+}
+class PostCandidateEvent extends CandidateEvents{
+  final Candidate candidate;
+  PostCandidateEvent({@required this.candidate});
+
+  @override
+  List<Object> get props => [];
+
+
+}
+class UpdateCandidateEvent extends CandidateEvents{
+  final Candidate candidate;
+  UpdateCandidateEvent({@required this.candidate});
+
+  @override
+  List<Object> get props => [];
+
+}
+class DeleteCandidateEvent extends CandidateEvents{
+  String id;
+  DeleteCandidateEvent({@required this.id});
+
+  @override
+  List<Object> get props => [];
+}
