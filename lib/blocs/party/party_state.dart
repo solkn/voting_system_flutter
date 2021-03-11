bdf4fb36b40b2f6db829ee10e1a3b10899e8cd52
solkn <solomonkindie52@gmail.com>
2021-03-11 11:02:31 +0300

    party_event.dart

---
+++ lib/blocs/party/party_state.dart
@@ -0,0 +1,50 @@
+
+import 'package:voting_system_flutter/models/model.dart';
+
+abstract class PartyStates{}
+
+class PartyUnintializedStae extends PartyStates{}
+
+class PartyFetchingState extends PartyStates{}
+
+class PartyFetchedState extends PartyStates{
+  final List<Party> party;
+
+  PartyFetchedState({this.party});
+}
+class PartyFetchingErrorState extends PartyStates{
+  final String message;
+
+  PartyFetchingErrorState({this.message});
+
+}
+class PartyPostingState extends PartyStates{}
+
+class PostedState extends PartyStates{}
+
+class PostingErrorState extends PartyStates{
+   final String message;
+   PostingErrorState({this.message});
+}
+
+class PartyUpdatingState extends PartyStates{}
+
+class PartyUpdatedState extends PartyStates{}
+
+class PartyUpdatingErrorState extends PartyStates{
+  final String message;
+
+  PartyUpdatingErrorState({this.message});
+}
+
+class PartyDeletingState extends PartyStates{}
+
+class PartyDeletedState extends PartyStates{}
+
+class PartyDeletingErrorState extends PartyStates{
+  final String message;
+  PartyDeletingErrorState({this.message});
+
+}
+
+class PartyEmptyState extends PartyStates{}
