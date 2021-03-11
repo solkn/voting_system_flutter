b059915fa25844a27df72c7b1e5ce95614c74a6c
solkn <solomonkindie52@gmail.com>
2021-03-12 00:02:11 +0300

    candidate_state.dart

---
+++ lib/blocs/candidate/candidate_state.dart
@@ -0,0 +1,50 @@
+
+import 'package:voting_system_flutter/models/model.dart';
+
+abstract class CandidateStates{}
+
+class CandidateUnintializedState extends CandidateStates{}
+
+class CandidateFetchingState extends CandidateStates{}
+
+class CandidateFetchedState extends CandidateStates{
+  final List<Candidate> candidate;
+
+  CandidateFetchedState({this.candidate});
+}
+class CandidateFetchingErrorState extends CandidateStates{
+  final String message;
+
+  CandidateFetchingErrorState({this.message});
+
+}
+class CandidatePostingState extends CandidateStates{}
+
+class CandidatePostedState extends CandidateStates{}
+
+class CandidatePostingErrorState extends CandidateStates{
+  final String message;
+  CandidatePostingErrorState({this.message});
+}
+
+class CandidateUpdatingState extends CandidateStates{}
+
+class CandidateUpdatedState extends CandidateStates{}
+
+class CandidateUpdatingErrorState extends CandidateStates{
+  final String message;
+
+  CandidateUpdatingErrorState({this.message});
+}
+
+class CandidateDeletingState extends CandidateStates{}
+
+class CandidateDeletedState extends CandidateStates{}
+
+class CandidateDeletingErrorState extends CandidateStates{
+  final String message;
+  CandidateDeletingErrorState({this.message});
+
+}
+
+class CandidateEmptyState extends CandidateStates{}
